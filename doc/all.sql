7d033516bcb6b6a59eed6012b4e3d5472f5b7008
zhigangwei <zhiw803@gmail.com>
2022-07-16 16:47:04 -0400

    3-5 set the mybatis generator and added demo table

--- doc/all.sql
+++ doc/all.sql
@@ -1,9 +1,22 @@
 drop table if exists `test`;
-create table `test` (
-    `id` bigint not null comment 'id',
-    `name` varchar(50) comment 'name',
+create table `test`
+(
+    `id`       bigint not null comment 'id',
+    `name`     varchar(50) comment 'name',
     `password` varchar(50) comment 'password',
     primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment = 'test';
+) engine = innodb
+  default charset = utf8mb4 comment = 'test';
 
-insert into `test` (id, name, password) values (1, 'test', password);
+insert into `test` (id, name, password)
+values (1, 'test', password);
+
+drop table if exists `demo`;
+create table `demo`
+(
+    `id`       bigint not null comment 'id',
+    `name`     varchar(50) comment 'name',
+    primary key (`id`)
+) engine = innodb default charset = utf8mb4 comment = 'demo';
+
+insert into `demo` (id, name) values (1, 'demo');
