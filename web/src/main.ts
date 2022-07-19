2ba5c88a5f88cfd36227f1c19227a0eb8aeffa93
zhigangwei <zhiw803@gmail.com>
2022-07-19 00:01:56 -0400

    5-6 add interceptors, print the log

--- web/src/main.ts
+++ web/src/main.ts
@@ -8,6 +8,25 @@ import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+
+/**
+ * axios interceptors
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('Request：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('Response：', response);
+    return response;
+}, error => {
+    console.log('Error：', error);
+    return Promise.reject(error);
+});
+
+
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
 
