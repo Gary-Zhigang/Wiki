478481a94790e2069e1a73bd5744a22cac01c784
zhigangwei <zhiw803@gmail.com>
2022-07-28 16:01:57 -0400

    6-9 added SnowFlake Util class, completed Add New function

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,6 +3,11 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <p>
+        <a-button type="primary" @click="add()" size="large">
+          Add New
+        </a-button>
+      </p>
       <a-table
           :columns="columns"
           :row-key="record => record.id"
@@ -166,6 +171,14 @@ export default defineComponent({
       ebook.value = record
     };
 
+    /**
+     * 新增
+     */
+    const add = () => {
+      modalVisible.value = true;
+      ebook.value = {};
+    };
+
 
     onMounted(() => {
       handleQuery({
@@ -182,6 +195,7 @@ export default defineComponent({
       handleTableChange,
 
       edit,
+      add,
       ebook,
       modalVisible,
       modalLoading,
