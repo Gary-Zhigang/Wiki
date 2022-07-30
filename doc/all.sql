6ade91703ea47a094529a015b3062e86bd8818d7
zhigangwei <zhiw803@gmail.com>
2022-07-29 22:39:09 -0400

    7-2 generated persistence layer code, basic infinite level tree structure design: id+parent

--- doc/all.sql
+++ doc/all.sql
@@ -1,4 +1,4 @@
-# Ebook Table
+# 电子书表
 drop table if exists `ebook`;
 create table `ebook` (
                          `id` bigint not null comment 'id',
@@ -19,27 +19,67 @@ insert into `ebook` (id, name, description) values (3, 'Python 入门教程', '�
 insert into `ebook` (id, name, description) values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用开发最佳首选框架');
 insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
 
-
-
 drop table if exists `test`;
-create table `test`
-(
-    `id`       bigint not null comment 'id',
-    `name`     varchar(50) comment 'name',
-    `password` varchar(50) comment 'password',
-    primary key (`id`)
-) engine = innodb
-  default charset = utf8mb4 comment = 'test';
+create table `test` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '名称',
+                        `password` varchar(50) comment '密码',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
 
-insert into `test` (id, name, password)
-values (1, 'test', password);
+insert into `test` (id, name, password) values (1, '测试', 'password');
+
+# 分类
+drop table if exists `category`;
+create table `category` (
+                            `id` bigint not null comment 'id',
+                            `parent` bigint not null default 0 comment '父id',
+                            `name` varchar(50) not null comment '名称',
+                            `sort` int comment '顺序',
+                            primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='分类';
+
+insert into `category` (id, parent, name, sort) values (100, 000, '前端开发', 100);
+insert into `category` (id, parent, name, sort) values (101, 100, 'Vue', 101);
+insert into `category` (id, parent, name, sort) values (102, 100, 'HTML & CSS', 102);
+insert into `category` (id, parent, name, sort) values (200, 000, 'Java', 200);
+insert into `category` (id, parent, name, sort) values (201, 200, '基础应用', 201);
+insert into `category` (id, parent, name, sort) values (202, 200, '框架应用', 202);
+insert into `category` (id, parent, name, sort) values (300, 000, 'Python', 300);
+insert into `category` (id, parent, name, sort) values (301, 300, '基础应用', 301);
+insert into `category` (id, parent, name, sort) values (302, 300, '进阶方向应用', 302);
+insert into `category` (id, parent, name, sort) values (400, 000, '数据库', 400);
+insert into `category` (id, parent, name, sort) values (401, 400, 'MySQL', 401);
+insert into `category` (id, parent, name, sort) values (500, 000, '其它', 500);
+insert into `category` (id, parent, name, sort) values (501, 500, '服务器', 501);
+insert into `category` (id, parent, name, sort) values (502, 500, '开发工具', 502);
+insert into `category` (id, parent, name, sort) values (503, 500, '热门服务端语言', 503);
+
+-- 文档表
+drop table if exists `doc`;
+create table `doc` (
+                       `id` bigint not null comment 'id',
+                       `ebook_id` bigint not null default 0 comment '电子书id',
+                       `parent` bigint not null default 0 comment '父id',
+                       `name` varchar(50) not null comment '名称',
+                       `sort` int comment '顺序',
+                       `view_count` int default 0 comment '阅读数',
+                       `vote_count` int default 0 comment '点赞数',
+                       primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='文档';
+
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (1, 1, 0, '文档1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (2, 1, 1, '文档1.1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (3, 1, 0, '文档2', 2, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (4, 1, 3, '文档2.1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (5, 1, 3, '文档2.2', 2, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (6, 1, 5, '文档2.2.1', 1, 0, 0);
 
 drop table if exists `demo`;
-create table `demo`
-(
-    `id`       bigint not null comment 'id',
-    `name`     varchar(50) comment 'name',
-    primary key (`id`)
-) engine = innodb default charset = utf8mb4 comment = 'demo';
+create table `demo` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '名称',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
 
-insert into `demo` (id, name) values (1, 'demo');
+insert into `demo` (id, name) values (1, '测试');
