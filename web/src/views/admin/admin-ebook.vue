3ba61016d301945a588b3e41adf46df9118ed8b3
zhigangwei <zhiw803@gmail.com>
2022-08-01 15:20:39 -0400

    8-3 created doc management page

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -37,6 +37,11 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
+            <router-link to="/admin/doc">
+              <a-button type="primary">
+                Doc Management
+              </a-button>
+            </router-link>
             <a-button type="primary" @click="edit(record)">
               Edit
             </a-button>
