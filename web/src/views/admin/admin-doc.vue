38bc567cd4fd3620a4962814e206977b42458db8
zhigangwei <zhiw803@gmail.com>
2022-08-01 20:06:47 -0400

    8-5 edited a document or added a new document with the id of the current ebook by using the tree selection component

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -59,17 +59,16 @@
         <a-input v-model:value="doc.name" />
       </a-form-item>
       <a-form-item label="Parent Doc">
-        <a-select
-          v-model:value="doc.parent"
-          ref="select"
+        <a-tree-select
+            v-model:value="doc.parent"
+            style="width: 100%"
+            :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+            :tree-data="treeSelectData"
+            placeholder="请选择父文档"
+            tree-default-expand-all
+            :replaceFields="{label: 'name', key: 'id', value: 'id'}"
         >
-          <a-select-option value="0">
-            None
-          </a-select-option>
-          <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="doc.id === c.id">
-            {{c.name}}
-          </a-select-option>
-        </a-select>
+        </a-tree-select>
       </a-form-item>
       <a-form-item label="Sort">
         <a-input v-model:value="doc.sort" />
@@ -83,10 +82,19 @@
   import axios from 'axios';
   import { message } from 'ant-design-vue';
   import {Tool} from "@/util/tool";
+  import {useRoute} from "vue-router";
 
   export default defineComponent({
     name: 'AdminDoc',
     setup() {
+      const route = useRoute();
+      console.log("路由：", route);
+      console.log("route.path：", route.path);
+      console.log("route.query：", route.query);
+      console.log("route.param：", route.params);
+      console.log("route.fullPath：", route.fullPath);
+      console.log("route.name：", route.name);
+      console.log("route.meta：", route.meta);
       const param = ref();
       param.value = {};
       const docs = ref();
@@ -131,6 +139,7 @@
        **/
       const handleQuery = () => {
         loading.value = true;
+        level1.value = [];
         axios.get("/doc/all").then((response) => {
           loading.value = false;
           const data = response.data;
@@ -148,6 +157,8 @@
       };
 
       // -------- 表单 ---------
+      const treeSelectData = ref();
+      treeSelectData.value = [];
       const doc = ref({});
       const modalVisible = ref(false);
       const modalLoading = ref(false);
@@ -166,6 +177,36 @@
           }
         });
       };
+      /**
+       * 将某节点及其子孙节点全部置为disabled
+       */
+      const setDisable = (treeSelectData: any, id: any) => {
+        // console.log(treeSelectData, id);
+        // 遍历数组，即遍历某一层节点
+        for (let i = 0; i < treeSelectData.length; i++) {
+          const node = treeSelectData[i];
+          if (node.id === id) {
+            // 如果当前节点就是目标节点
+            console.log("disabled", node);
+            // 将目标节点设置为disabled
+            node.disabled = true;
+
+            // 遍历所有子节点，将所有子节点全部都加上disabled
+            const children = node.children;
+            if (Tool.isNotEmpty(children)) {
+              for (let j = 0; j < children.length; j++) {
+                setDisable(children, children[j].id)
+              }
+            }
+          } else {
+            // 如果当前节点不是目标节点，则到其子节点再找找看。
+            const children = node.children;
+            if (Tool.isNotEmpty(children)) {
+              setDisable(children, id);
+            }
+          }
+        }
+      };
 
       /**
        * 编辑
@@ -173,6 +214,13 @@
       const edit = (record: any) => {
         modalVisible.value = true;
         doc.value = Tool.copy(record);
+
+        // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
+        treeSelectData.value = Tool.copy(level1.value);
+        setDisable(treeSelectData.value, record.id);
+
+        // 为选择树添加一个"无"
+        treeSelectData.value.unshift({id: 0, name: 'None'});
       };
 
       /**
@@ -180,7 +228,15 @@
        */
       const add = () => {
         modalVisible.value = true;
-        doc.value = {};
+        doc.value = {
+          ebookId: route.query.ebookId
+        };
+
+        treeSelectData.value = Tool.copy(level1.value);
+
+        // 为选择树添加一个"无"
+        treeSelectData.value.unshift({id: 0, name: 'None'});
+
       };
 
       const handleDelete = (id: number) => {
@@ -213,7 +269,8 @@
         modalLoading,
         handleModalOk,
 
-        handleDelete
+        handleDelete,
+        treeSelectData
       }
     }
   });
